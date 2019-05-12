ac1886a4223ba45b50613704e4cebc7b4c5bd8ce
David <davidmaccurso@gmail.com>
2019-05-12 17:28:33 -0300

    authors

--- src/app/autores/autores.component.ts
+++ src/app/autores/autores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostServiceComponent } from '../post-service/post-service.component';
 
 @Component({
   selector: 'app-autores',
@@ -7,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutoresComponent implements OnInit {
 
-  constructor() { 
+  constructor(
+    private postService : PostServiceComponent
+  ) { 
     this.imgPath = "../assets/apu.jpg";
   }
 
   ngOnInit() {
+    // this.authorPosts = this.postService.GetAllPostsFromAuthor(1);
   }
   imgPath: string;
+  authorPosts: Post[];
+
 }
